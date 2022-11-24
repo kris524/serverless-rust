81d90fda3ba3caa0c40689f44c2b6db052a767f0
Kris <kristian_dilov@abv.bg>
2022-11-24 23:59:26 +0000

    first commit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@
 // get the curerntly configured aws chain
 use aws_config::meta::region::RegionProviderChain;
 
+// dynamodb part of the sdk
+use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::model::AttributeValue;
+
+//the lambda runtime that will help us tell lambda how to call our handler function
+use lambda_runtime::{handler_fn, Context, Error as LambdaError};
+
+// serde lib for deceralisation
+use serde::Deserialize;
+
+
+
 
 
 fn main() {
