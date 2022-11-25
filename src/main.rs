dde133fa79f77a404b9c0d13565fdec8f427bb7c
Kris <kristian_dilov@abv.bg>
2022-11-25 08:13:45 +0000

    first commit

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,16 @@ use lambda_runtime::{handler_fn, Context, Error as LambdaError};
 // serde lib for deceralisation
 use serde::Deserialize;
 
+use uuid::Uuid;
 
 
 
-
-fn main() {
-   
+#[tokio::main]
+async fn main() -> Result<(),LambdaError> {
+   let func = handler_fn(handler);
+   lambda_runtime::run(func).await?; //block until the results come back
+        // the ? will get us the caller of the main function 
+    Ok(())
 }
+
+//set up the custom event object 
